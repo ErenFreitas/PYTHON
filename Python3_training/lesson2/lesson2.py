a7528a7e6fd0e715b2b428799fe737c8f9d87d71
Eren Freitas <yuukirito200303u@gmail.comgit config --global user.email yuukirito200303@gmail.com>
2024-03-20 21:07:44 -0300

    commit2

---
+++ Python3_training/lesson2/lesson2.py
@@ -0,0 +1,31 @@
+
+
+
+#In Python, the int type represents integer numbers, which can be positive or negative. Unsigned integers are considered positive.
+
+print(11) #int
+print(-11) #int
+print(0) #int
+
+#In Python, the float type represents numbers with floating-point precision. It can represent any positive or negative number with decimal points
+
+print(1.1) #float
+print(-11.11) #float
+print(0.1) #float
+
+#In Python, str is a data type that represents a sequence of characters, or in other words, a string of text
+
+# Creating strings
+string1 = 'Hello, world!'
+string2 = "This is a string."
+
+# Printing strings
+print(string1)  # Output: Hello, world!
+print(string2)  # Output: This is a string.
+
+#In Python, the type() function is used to return the type of an object. It takes a single argument and returns the type of that argument
+
+print(type(5))  # Output: <class 'int'>
+
+print(type("Hello, world!"))  # Output: <class 'str'>
+
