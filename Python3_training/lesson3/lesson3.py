89eb48272f9f179eec520d3087f67600a1089257
Eren Freitas <yuukirito200303u@gmail.comgit config --global user.email yuukirito200303@gmail.com>
2024-03-25 17:07:29 -0300

    commit2

---
+++ Python3_training/lesson3/lesson3.py
@@ -0,0 +1,20 @@
+# Defining some boolean variables
+true = True
+false = False
+
+# Printing the values of the variables
+print("The value of true is:", true)
+print("The value of false is:", false)
+
+# Converting integer to float
+integer_num = 5
+float_num = float(integer_num)
+print("Integer " + str(integer_num) + " converted to float: " + str(float_num))
+
+# Converting float to integer
+float_num = 3.14
+integer_num = int(float_num)
+print("Float " + str(float_num) + " converted to integer: " + str(integer_num))
+
+
+
