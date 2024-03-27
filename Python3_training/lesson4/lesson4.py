12cfdeeee219e5b07b050c162f0a561524116a48
Eren Freitas <yuukirito200303u@gmail.comgit config --global user.email yuukirito200303@gmail.com>
2024-03-27 15:52:03 -0300

    commit3

---
+++ Python3_training/lesson4/lesson4.py
@@ -0,0 +1,33 @@
+# Arithmetic operators in Python
+
+# Defining two variables for operations
+a = 10
+b = 3
+
+# Addition
+addition_result = a + b
+print("Addition:", addition_result)
+
+# Subtraction
+subtraction_result = a - b
+print("Subtraction:", subtraction_result)
+
+# Multiplication
+multiplication_result = a * b
+print("Multiplication:", multiplication_result)
+
+# Division
+division_result = a / b
+print("Division:", division_result)
+
+# Integer division
+integer_division_result = a // b
+print("Integer Division:", integer_division_result)
+
+# Modulo (Remainder of division)
+modulo_result = a % b
+print("Modulo (Remainder of Division):", modulo_result)
+
+# Exponentiation
+exponentiation_result = a ** b
+print("Exponentiation:", exponentiation_result)
